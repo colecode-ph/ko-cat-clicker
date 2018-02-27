bcaf96efa213efecee7c71daca74eb138c7e3770
Tom Cole <colecode.ph@gmail.com>
2018-02-28 01:16:46 +0800

    Clean up nickname array and use $data in the li instead of keys

--- js/app.js
+++ js/app.js
@@ -1,12 +1,7 @@
 var ViewModel = function () {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
-    this.nicknames = ko.observable([
-        {nickname:'idiot'},
-        {nickname: 'stinky'},
-        {nickname: 'stupid'},
-        {nickname: 'smelly'}
-        ]);
+    this.nicknames = ko.observable(['idiot', 'stupid', 'stinky', 'shithead']);
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('WTF?');
     this.incrementCounter = function () {
