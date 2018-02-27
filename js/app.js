994118ffcab9dd9735657810c4edb97084684c7e
Tom Cole <colecode.ph@gmail.com>
2018-02-28 01:14:10 +0800

    Get rid of the stupid 'level' stuff

--- js/app.js
+++ js/app.js
@@ -9,13 +9,6 @@ var ViewModel = function () {
         ]);
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('WTF?');
-    this.level = ko.computed( function() {
-        if (this.clickCount() >= 3) {
-            return "Toddler";
-        } else {
-            return "Baby";
-        }
-    }, this);
     this.incrementCounter = function () {
         this.clickCount(this.clickCount() + 1);
     };
