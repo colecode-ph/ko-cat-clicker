170b54f0bc8b895a0176d0fd0420302a8b2c63d7
Tom Cole <colecode.ph@gmail.com>
2018-02-28 17:14:25 +0800

    Separate data from ViewModel by creating Cat() function

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
-var ViewModel = function () {
+var Cat = function () {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.nicknames = ko.observable(['idiot', 'stupid', 'stinky', 'shithead']);
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('WTF?');
+};
+
+var ViewModel = function () {
+    this.currentCat = ko.observable( new Cat() );
+
     this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
     };
-
-
 }
 
 ko.applyBindings(new ViewModel());
