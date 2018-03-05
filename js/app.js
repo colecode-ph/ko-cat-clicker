38547e7481b9ba104da3d4b10dca6f369a5ffa2f
Tom Cole <colecode.ph@gmail.com>
2018-03-05 21:53:55 +0800

    Add another cat(s), turn them in to an array of objects
    initialCats is the array of objects which now contains cat data

--- js/app.js
+++ js/app.js
@@ -1,24 +1,47 @@
-var Cat = function () {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.nicknames = ko.observable(['idiot', 'stupid', 'stinky', 'shithead']);
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+var initialCats = [
+    {
+        clickCount: 0,
+        name: 'Tabby',
+        nicknames: ['idiot', 'stupid', 'stinky', 'shithead'],
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    },
+    {
+        clickCount: 0,
+        name: 'Stupid',
+        nicknames: ['idiot', 'stupid', 'stinky', 'shit-for-brains'],
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    },
+
+];
+
+
+var Cat = function (data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.nicknames = ko.observable(data.nicknames);
+    this.imgSrc = ko.observable(data.imgSrc);
 };
 
 var ViewModel = function () {
-    // var self = this;
+    var self = this;
     // 'this refers to the ViewModel binding context'
 
-    this.currentCat = ko.observable( new Cat() );
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem){
+        self.catList.push( new Cat(catItem) );
+    });
+
+    this.currentCat = ko.observable( this.catList()[0] );
 
     this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
+        // this.clickCount(this.clickCount() + 1);
         // 'this' refers to the currentCat binding context
         // because of the with: currentCat binding on the div element
         // which I don't understand because I thought $parent escaped that context
 
-        // another way to do this using the self varible
-        // self.currentCat().clickCount(this.currentCat().clickCount() + 1);
+        // another way to do this using the self varible at the top of the function
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
 }
 
